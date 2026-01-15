f2563a65418b6e74b6ba07919e80854323adf398
carlos676767 <carlitobr780@yahoo.com>
2026-01-15 17:37:05 -0400

    Path join implemented in Backup Files process.

--- sync-engine/core/service/viewArchiles.js
+++ sync-engine/core/service/viewArchiles.js
@@ -1,16 +1,17 @@
-import path from "node:path";
+
 import copyFilesToStorage from "./RemovedSArchives.js";
 import readFilesFromFolder from "../utils/readFile.js";
 
 import checkSizeDesktop from "./checkDiskService.js";
 import zipItem from "./zipFiles.js";
+import pathJoins from "../utils/joinPats.js";
 
 export default async function processBackupFiles() {
   const hardDisk = await checkSizeDesktop();
-  const backupFolder = path.join(hardDisk, "backupApp");
+  const backupFolder = pathJoins(hardDisk, "backupApp");
   const backupFiles = await readFilesFromFolder(backupFolder);
 
-  const storageFolder = path.join("storage");
+  const storageFolder = pathJoins(`storage`);
   const storageFiles = await readFilesFromFolder(storageFolder);
 
   const filesNotInStorage = backupFiles.filter(
--- sync-engine/core/utils/joinPats.js
+++ sync-engine/core/utils/joinPats.js
@@ -1,5 +1,5 @@
 
 import path from "node:path";
-export default function pathJoins(url) {
-    return path.join(url)
+export default function pathJoins(url,url2 = ``) {
+    return path.join(url, url2)
 }
